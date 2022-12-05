1dc4ff83c377ed4f1295b8cbd023b9f3f8c0b4ff
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-05 12:03:53 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine($"{customer.CustomerName} {customer.CustomerSurName} İsimli Müşteri Sisteme Eklendi.");
+        }        
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine($"{customer.CustomerName} {customer.CustomerSurName} İsimli Müşteri Sistemden Silindi.");
+        }        
+        public void Update(Customer customer)
+        {
+            Console.WriteLine($"{customer.CustomerName} {customer.CustomerSurName} İsimli Müşteri Güncellendi.");
+        }        
+        public void Select(Customer customer)
+        {
+            Console.WriteLine($"Müşteri Bilgileri {customer.Id} {customer.CustomerName} {customer.CustomerSurName} ");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.CustomerName = "Özgür";
+            customer.CustomerSurName = "Rüzgar";    
+            
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.CustomerName = "Engin";
+            customer2.CustomerSurName = "Demiroğ";    
+            
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.CustomerName = "Mert";
+            customer3.CustomerSurName = "Aktaş";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer);
+            customerManager.Update(customer2);
+            customerManager.Delete(customer3);
+            customerManager.Select(customer3);
+        }
+    }
+}
